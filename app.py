926b7cd229fd915b489c622630e54b0a3b81a899
SudarshanAhire <sudarshanahire292@gmail.com>
2025-08-14 21:35:36 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,53 +1,127 @@
-from flask import Flask,request,render_template
+# from flask import Flask,request,render_template
+# import numpy as np
+# import pandas
+# import sklearn
+# import pickle
+
+# # importing model
+# model = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\model.pkl', 'rb'))
+# sc = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\standscaler.pkl', 'rb'))
+# ms = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\minmaxscaler.pkl', 'rb'))
+
+# # creating flask app
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
+
+# @app.route("/predict",methods=['POST'])
+# def predict():
+#     N = request.form['Nitrogen']
+#     P = request.form['Phosphorus']
+#     K = request.form['Potassium']
+#     temp = request.form['Temperature']
+#     humidity = request.form['Humidity']
+#     ph = request.form['Ph']
+#     rainfall = request.form['Rainfall']
+
+#     feature_list = [N, P, K, temp, humidity, ph, rainfall]
+#     single_pred = np.array(feature_list).reshape(1, -1)
+
+#     scaled_features = ms.transform(single_pred)
+#     final_features = sc.transform(scaled_features)
+#     prediction = model.predict(final_features)
+
+#     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+#                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+#                  14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+#                  19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+#     if prediction[0] in crop_dict:
+#         crop = crop_dict[prediction[0]]
+#         result = "{} is the best crop to be cultivated right there".format(crop)
+#     else:
+#         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+#     return render_template('index.html',result = result)
+
+
+
+
+# # python main
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+from flask import Flask, request, render_template
 import numpy as np
-import pandas
-import sklearn
 import pickle
 
-# importing model
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+# Importing model and scalers
+model = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\model.pkl', 'rb'))
+sc = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\standscaler.pkl', 'rb'))
+ms = pickle.load(open(r'c:\Users\Sudarshan Ahire\OneDrive\Desktop\ppppppppp\Crop-Recommendation-System-Using-Machine-Learning\minmaxscaler.pkl', 'rb'))
 
-# creating flask app
+# Creating flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/predict",methods=['POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
-    N = request.form['Nitrogen']
-    P = request.form['Phosporus']
-    K = request.form['Potassium']
-    temp = request.form['Temperature']
-    humidity = request.form['Humidity']
-    ph = request.form['Ph']
-    rainfall = request.form['Rainfall']
+    # Collecting input values
+    N = float(request.form['Nitrogen'])
+    P = float(request.form['Phosphorus'])
+    K = float(request.form['Potassium'])
+    temp = float(request.form['Temperature'])
+    humidity = float(request.form['Humidity'])
+    ph = float(request.form['Ph'])
+    rainfall = float(request.form['Rainfall'])
 
+    # Debugging: Print input values
+    print(f"Inputs: N={N}, P={P}, K={K}, Temperature={temp}, Humidity={humidity}, pH={ph}, Rainfall={rainfall}")
+
+    # Creating feature array and reshaping it for the model
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
     single_pred = np.array(feature_list).reshape(1, -1)
 
+    # Debugging: Print before scaling
+    print("Before scaling:", single_pred)
+
+    # Scaling input values
     scaled_features = ms.transform(single_pred)
     final_features = sc.transform(scaled_features)
+
+    # Debugging: Print after scaling
+    print("After scaling:", final_features)
+
+    # Making prediction
     prediction = model.predict(final_features)
 
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+    # Debugging: Print raw prediction
+    print("Raw prediction:", prediction)
 
+    # Mapping prediction to crop name
+    crop_dict = {
+        1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+        8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+        14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+        19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"
+    }
+
+    # Determining the result
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
         result = "{} is the best crop to be cultivated right there".format(crop)
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html',result = result)
+
+    # Returning result to the UI
+    return render_template('index.html', result=result)
 
 
-
-
-# python main
+# Python main
 if __name__ == "__main__":
     app.run(debug=True)
